ead300bd956888519b02e752e364708b57fb65df
Maksym Korostelov <flying1519@gmail.com>
2023-12-22 21:07:46 +0200

    catalog in progress

--- app/shop/catalog/catalog.jsx
+++ app/shop/catalog/catalog.jsx
@@ -4,50 +4,69 @@ import axios from "axios";
 import CatalogNavigation from "./navigation/catalogNavigation";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import { asicCatalog } from "@/lib/features/shopCatalogSlice";
+import { gpuCatalog } from "@/lib/features/shopCatalogSlice";
+import { hardCatalog } from "@/lib/features/shopCatalogSlice";
+import { useAppDispatch } from "@/lib/hooks";
+import { useAppSelector } from "@/lib/hooks";
 
 const Catalog = () => {
   //get-catalog-items----------------------------------------------------------
   const [catalogItems, setCatalogItems] = useState([]);
-  const [itemsImages,setItemsImages] = useState([])
-  console.log(itemsImages)
+  const [itemsImages, setItemsImages] = useState([]);
+  const [stylePopularity, setStylePopularity] = useState(false);
+ //switch-catalogs----------------------------------------------------------
+ const currentComponent = useAppSelector((state) => state.shopCatalog.currentComponent )
+ const dispath = useAppDispatch()
+ const handleAsicCatalog = () => {
+  dispath(asicCatalog())
+ }
+ const handleGpuCatalog = () => {
+  dispath(gpuCatalog())
+ }
+ const handleHardCatalog = () => {
+  dispath(hardCatalog())
+ }
   async function getCatalogItems() {
     try {
       const response = await axios.get(
         "https://quitystrapi.onrender.com/api/catalog-items?populate=*"
       );
       const dataResponse = response.data.data;
-      const dataResponseImages = dataResponse.map((item) => item.attributes.goodImage.data.attributes.url)
+      const dataResponseImages = dataResponse.map(
+        (item) => item.attributes.goodImage.data.attributes.url
+      );
       if (dataResponseImages) {
         setCatalogItems(dataResponse);
-        setItemsImages(dataResponseImages)
+        setItemsImages(dataResponseImages);
       }
-      console.log(dataResponse)
-    } catch (error) {
+     } catch (error) {
       console.error("get catalog items are failed");
     }
   }
   useEffect(() => {
     getCatalogItems();
   }, []);
- 
+
   return (
     <div className={c.wrapper}>
       <div className={c.catalog__body}>
         <h4 className={c.title}>Более 120 моделей в наличии и под заказ</h4>
-        <CatalogNavigation />
+        <CatalogNavigation clickAsic={handleAsicCatalog} clickGpu={handleGpuCatalog} clickHard={handleHardCatalog}/>
+        {currentComponent === "asicCatalog" && (
         <div className={c.goods__wrapper}>
-          {catalogItems.map((item,index) => (
+         {catalogItems.map((item, index) => (
             <div className={c.item} key={index}>
               <div className={c.image__wrapper}>
-                 <Image
+                <Image
                   src={itemsImages[index]}
                   width={182}
                   height={168}
                   className={c.minerImage}
-                /> 
+                />
               </div>
               <div className={c.description}>
-                <span className={c.presence}></span>
+                <span className={c.presence}>{item.attributes.presence}</span>
                 <span className={c.title}>{item.attributes.title}</span>
                 <div className={c.parameters}>
                   <div className={c.parameters__item}>
@@ -55,11 +74,11 @@ const Catalog = () => {
                     <span className={c.text}>TH/s</span>
                   </div>
                   <div className={c.parameters__item}>
-                    <span className={c.numbers}></span>
+                    <span className={c.numbers}>{item.attributes.w}</span>
                     <span className={c.text}>W</span>
                   </div>
                   <div className={c.parameters__item}>
-                    <span className={c.numbers}></span>
+                    <span className={c.numbers}>{item.attributes.jth}</span>
                     <span className={c.text}>J/TH</span>
                   </div>
                 </div>
@@ -68,10 +87,16 @@ const Catalog = () => {
                   <span className={c.priceIcon}>$</span>
                 </div>
               </div>
-              <span className={c.label}></span>
+              <span  className={item.attributes.popularity === "Новинка" ? c.label : c.labelHit}>{item.attributes.popularity}</span>
             </div>
           ))}
-        </div>
+       </div>)
+        }
+        {/* //gpu-catalog------------------------------------------------------------------------------------- */}
+        {currentComponent === "gpuCatalog" && <div>gpu</div>}
+      
+         {/* //hard-catalog------------------------------------------------------------------------------------- */}
+         {currentComponent === "hardCatalog" && (<div>hard</div>)}
       </div>
     </div>
   );
--- app/shop/catalog/navigation/catalogNavigation.jsx
+++ app/shop/catalog/navigation/catalogNavigation.jsx
@@ -2,20 +2,20 @@
 import c from '../catalog.module.scss';
 import { useState } from 'react';
 
-const CatalogNavigation = () => {
+const CatalogNavigation = (props) => {
     const [activeIndex,setActiveIndex] = useState(0)
     const switchIndex = (index) => {
        setActiveIndex(index)
     }
     return (
         <div className={c.navigation}>
-        <div className={activeIndex === 0 ? c.activeCatalog : c.passiveCatalog} onClick={() => switchIndex(0)}>
+        <div className={activeIndex === 0 ? c.activeCatalog : c.passiveCatalog} onClick={() => {switchIndex(0);props.clickAsic()}}>
         ASIC–майнеры
         </div>
-        <div className={activeIndex === 1 ? c.activeCatalog : c.passiveCatalog} onClick={() => switchIndex(1)}>
+        <div className={activeIndex === 1 ? c.activeCatalog : c.passiveCatalog} onClick={() => {switchIndex(1);props.clickGpu()}}>
         GPU–майнеры
         </div>
-        <div className={activeIndex === 2 ? c.activeCatalog : c.passiveCatalog} onClick={() => switchIndex(2)}>
+        <div className={activeIndex === 2 ? c.activeCatalog : c.passiveCatalog} onClick={() => {switchIndex(2);props.clickHard();}}>
         Жесткие диски
         </div>
      </div>
