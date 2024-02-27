5c445ab3840b29d7dc90569e8f324c1f6490268f
Maksym Korostelov <flying1519@gmail.com>
2024-02-27 19:09:38 +0200

    typof delete

--- app/about/page.js
+++ app/about/page.js
@@ -13,15 +13,11 @@ import { AboutData } from "@/lib/features/about/aboutSlice";
 import Loading from "../components/loading/loading";
 
 export default function AboutPage() {
-  const [isBrowser, setIsBrowser] = useState(false);
-  useEffect(() => {
-    setIsBrowser(typeof window !== 'undefined');
-  }, []);
   const currentComponent = useAppSelector(
     (state) => state.about.currentComponent
   );
   //-----------------------------------------------------------------------
-   
+
   //isLoading---------------------------------------------------
   const [isLoading, setIsLoading] = useState(true);
   //render-content-if-value----------------------------------------
@@ -34,39 +30,32 @@ export default function AboutPage() {
 
   return (
     <>
-    {isBrowser &&  <div className={a.wrapper}>
-        <AboutData />
-        {isLoading ? (
-          <Loading />
+      <AboutData />
+      {isLoading ? (
+        <Loading />
       ) : (
-          <div className="container">
-            <div className={a.about__body}>
+        <div className="container">
+          <div className={a.about__body}>
             {/* Проверяем наличие window перед использованием */}
-            {typeof window !== 'undefined' && (
-              <div
-                  className={a.fixed}
-                >
-                  <AboutNavigation className={a.aboutNavigationComponent} />
-                </div>
-            )}
-              {currentComponent === "about" && <About className={a.content} />}
-              {currentComponent === "requisite" && (
-                <Requisites className={a.content} />
-              )}
-              {currentComponent === "contacts" && (
-                <Contacts className={a.content} />
-              )}
-              {currentComponent === "publicOffer" && (
-                <PublicOffer className={a.content} />
-              )}
-              {currentComponent === "agreement" && (
-                <Agreement className={a.content} />
-              )}
+            <div className={a.fixed}>
+              <AboutNavigation className={a.aboutNavigationComponent} />
             </div>
+            {currentComponent === "about" && <About className={a.content} />}
+            {currentComponent === "requisite" && (
+              <Requisites className={a.content} />
+            )}
+            {currentComponent === "contacts" && (
+              <Contacts className={a.content} />
+            )}
+            {currentComponent === "publicOffer" && (
+              <PublicOffer className={a.content} />
+            )}
+            {currentComponent === "agreement" && (
+              <Agreement className={a.content} />
+            )}
           </div>
-        )}
-      </div>}
-     
+        </div>
+      )}
     </>
   );
 }
